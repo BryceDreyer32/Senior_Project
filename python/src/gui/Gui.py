c2f06429f1a01918b287ab52f6be6e1d651413c1
BryceDreyer32 <bryce.b.dreyer@gmail.com>
2025-04-30 10:54:31 +0800

    Moving pictures. Updates to code to load GUI

---
+++ python/src/gui/Gui.py
@@ -0,0 +1,18 @@
+# Copyright 2024
+# Bryce's Senior Project
+# Description: This is the main entry point of the Robot code
+from PyQt5 import QtWidgets, uic
+from PyQt5 import uic
+import sys, os
+
+class Gui(QtWidgets.QDialog):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        print("cwd = " + str(os.getcwd()))
+
+        # Load the UI Page - added path too
+        ui_path = os.path.dirname(os.path.abspath(__file__))
+        uic.loadUi("GUIs/PyQt5/Rev2_Onyx.ui", self)
+
